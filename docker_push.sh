8779a0f759d007075d89d6dd2001fc25a7077da8
Pauli Jokela <pauli.jokela@didstopia.com>
2016-11-14 12:31:17 +0200

    Pushing changes

--- docker_push.sh
+++ docker_push.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker tag -f didstopia/docker-plexconnect:latest didstopia/docker-plexconnect:latest
+docker tag didstopia/docker-plexconnect:latest didstopia/docker-plexconnect:latest
 docker push didstopia/docker-plexconnect:latest
